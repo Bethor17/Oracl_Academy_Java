fbe4f42c9ca363348e3a6150877904e37c33cfbc
Bethor17 <60106107+Bethor17@users.noreply.github.com>
2020-11-05 23:28:40 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.tcca;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+	}//main
+
+}//class
